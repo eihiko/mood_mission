d8411026ea5f04caa911588f9ef44c7f6f5b5dd3
ShaunHoward <smh150@case.edu>
2015-03-25 11:09:08 -0400

    Update MissionEvent.cs

--- Assets/CTSC Game/Scripts/MissionEvent.cs
+++ Assets/CTSC Game/Scripts/MissionEvent.cs
@@ -137,6 +137,7 @@ public class MissionEvent : MonoBehaviour {
 			actionQ.Enqueue(new StandAction(Torkana, inFrontTorkanaHouse));
 			//Player must ENTER(Forest)
 			actionQ.Enqueue(new EnterAction(Player, Forest));
+			//Checkpoint to reflect with gui and input, write data to database
 			break;
 		//Mission Two events
 		case MissionManager.EventType.LEAVE_GUIDES:
@@ -171,6 +172,7 @@ public class MissionEvent : MonoBehaviour {
 			//Player must MOVE(currLoc, adjToDoctorsHouse)
 			//Player must ENTER(DoctorsHouse)
 			//Torkana must STAND(adjToDoctor) in the house
+			//Checkpoint to reflect with gui and input, write data to database
 			break;
 		//Mission Three events
 		case MissionManager.EventType.ENTER_DOCTORS:
@@ -209,7 +211,7 @@ public class MissionEvent : MonoBehaviour {
 			//Torkana must TALK(audio, noGui)
 			//Player must MOVE(currLoc, adjToDoctor)
 			//Doctor must TALK(audio, guiToShow)
-		    //Player must GIVE(Herb, Doctor)
+		        //Player must GIVE(Herb, Doctor)
 			//Doctor must TALK(audio, guiToShow)
 				break;
 		case MissionManager.EventType.PLAYER_EXAM:
@@ -223,6 +225,7 @@ public class MissionEvent : MonoBehaviour {
 			//Torkana must MOVE(currLoc, adjToDoor)
 			//Torkana must ENTER(Forest)
 			//Torkana must ACTIVE(false)
+			//Checkpoint to reflect with gui and input, write data to database
 			break;
 		//Mission 4 Actions
 		case MissionManager.EventType.LEAVE_FOREST:

b752aa50af2475e4384302a9703d371272e610a1
Evelyn Moss <evelynmoss.szf@gmail.com>
2015-05-19 17:08:49 -0400

    Move unused scenes and scripts to separate folder
    Went through and found the scenes, scripts, etc. that weren't being used
    and moved them to a separate folder so they don't clog things up.

---
+++ Assets/Unused Stuff/testScript.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class testScript : MonoBehaviour {
+
+	private bool hasCalledDialog;
+	public GameObject dialog;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+	}
+
+	void OnTriggerEnter(Collider other) 
+	{
+		if (!hasCalledDialog) {
+			this.dialog.GetComponent<DialogBox> ().displayText (true, 0, 3);
+		}
+		this.dialog.GetComponent<PlayerStatusBars> ().DecreaseStat(30, "Compassion");
+	}
+}
